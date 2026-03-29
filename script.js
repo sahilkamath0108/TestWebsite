bf2b70f4a09d952119e1353f2defadf495f5ae68
Sahil Kamath <71258959+sahilkamath0108@users.noreply.github.com>
2026-03-28 20:10:22 -0400

    Add script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// You can add JavaScript functionality here if needed.
+// For example, to make the CTA buttons interactive.
+
+document.querySelector(".cta-button").addEventListener("click", () => {
+    alert("Get Started button clicked!");
+});
+
+document.querySelector(".main-cta").addEventListener("click", () => {
+    alert("Learn More button clicked!");
+});
